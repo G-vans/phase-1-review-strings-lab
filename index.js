642934a45f128ac985497c185c99f16399c950e5
G-vans <jevans.otieno@student.moringaschool.com>
2022-08-16 16:04:08 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your code in this file!
+
+//Current User variable
+let currentUser = "Jevans Otieno";
+
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}` + '!';
+
+// const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+
+console.log( welcomeMessage );
+
+
+//Excited Welcome Message Variable
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+let upperCaseCurrentUser = currentUser.toUpperCase();
+
+console.log(upperCaseCurrentUser);
+
+
+
+//Short Greeting variable
+let shortGreeting = 'Welcome, ';
+
+const firstInitial = currentUser[0];
+const restOfName = currentUser.slice(0,1);
+
+// console.log(firstInitial);
+console.log(restOfName)
+
+shortGreeting = `Welcome, ${restOfName}!`;
+
+console.log(shortGreeting);
